6d5812cdeafee75e5c6ca529b817e7c538defaee
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-06-10 22:11:01 +0200

    Change http on https in Contact.js

--- components/Contact.js
+++ components/Contact.js
@@ -6,7 +6,7 @@ var Contact = React.createClass({
   render: function() {
     return (
       React.createElement('div',{className: 'contactItem'},
-        React.createElement('img', {className: 'contactImage', src: 'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}),
+        React.createElement('img', {className: 'contactImage', src: 'https://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}),
         React.createElement('p', {className: 'contactLabel'}, 'Name: ' + this.props.item.firstName),
         React.createElement('p', {className: 'contactLabel'}, 'Surname: ' + this.props.item.lastName),
         React.createElement('a', {className: 'contactEmail', href: "Email " + this.props.item.email}, 'Email: ' + this.props.item.email)

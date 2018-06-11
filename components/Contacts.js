d5edc88d28aff94227f8a3322b261745515194a4
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-06-11 19:29:34 +0200

    Fix the bug in Contacts.js

--- components/Contacts.js
+++ components/Contacts.js
@@ -4,10 +4,12 @@ var Contacts = React.createClass({
 //  in this array is a list of contacs from App.js
     items: React.PropTypes.array.isRequired,
   },
+  // mapping the contact table -> changing the contacts on React elements
   render: function(){
     var contacts = this.props.items.map(function(contact) {
       return React.createElement(Contact, {item: contact, key: contact.id});
-
+    }),
+  // create list <ul> and insert React elements created on line 10
     return (
       React.createElement('ul', {className: 'contactList'}, contacts)
     );
